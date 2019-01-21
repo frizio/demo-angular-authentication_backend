f9bbed90492319c4990338ab145d7f4be8deb003
Maurizio La Rocca <mrz.larocca@gmail.com>
2019-01-21 15:33:56 +0100

    Set up database connection

--- src/routes/api.js
+++ src/routes/api.js
@@ -1,6 +1,23 @@
 const { Router } = require('express');
 const router = Router();
 
+const mongoose = require("mongoose");
+const dbUrl = "mongodb://user:password@localhost:27017/test";
+
+mongoose.connect(
+    dbUrl,
+    (err) => {
+        if (err) {
+            console.log("There is an Error:\n" + err);
+        }
+        else {
+            console.log("Application connected to mongodb");
+        }
+    }
+);
+
+
+
 router.get(
     '/',
     (req, res) => {
