961e1addf94757e00f0d5bd2f585c2f3f954e5c6
Maurizio La Rocca <mrz.larocca@gmail.com>
2019-01-21 16:38:37 +0100

    Create Login API

--- src/routes/api.js
+++ src/routes/api.js
@@ -52,5 +52,36 @@ router.post(
     }
 );
 
+router.post(
+    '/login',
+    (req, res) => {
+        console.log("Post on route /login");
+        let userData = req.body;
+        User.findOne(
+            // Check username
+            {email: userData.email},
+            (err, user) => {
+                if (err) {
+                    console.log("There is an error");
+                    console.log(err);
+                } else {
+                    if (!user) {
+                        console.log("The User is unknowing");
+                        res.status(401).send("Username invalid"); 
+                    } else {
+                        // The user exist... Check the password
+                        if (user.password != userData.password ) {
+                            console.log("The password is invalid");
+                            res.status(401).send("Password invalid"); 
+                        } else {
+                            console.log("Login user in the system");
+                            res.status(200).send(user); 
+                        }
+                    }
+                }
+            }
+        );
+    }
+);
 
 module.exports = router;
