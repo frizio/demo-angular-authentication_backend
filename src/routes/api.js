1bcec8da1eb8b3ef1dfd81dc2bf818c8e8b139a5
Maurizio La Rocca <mrz.larocca@gmail.com>
2019-01-21 15:53:52 +0100

    Create registration API

--- src/routes/api.js
+++ src/routes/api.js
@@ -1,7 +1,8 @@
 const { Router } = require('express');
 const router = Router();
-
 const mongoose = require("mongoose");
+const User = require('../models/user');
+
 const dbUrl = "mongodb://user:password@localhost:27017/test";
 
 mongoose.connect(
@@ -25,5 +26,27 @@ router.get(
         res.send('Hello from server');
     }
 );
+
+router.post(
+    '/register',
+    (req, res) => {
+        console.log("Post on route /register");
+        let userData = req.body;
+        let user = new User(userData);
+        // Saving
+        user.save(
+            (err, registredUser) => {
+                if (err) {
+                    console.log("There is an error");
+                    console.log(err);
+                } else {
+                    console.log("User registed correctly");
+                    res.status(200).send(registredUser);
+                }
+            }
+        );
+    }
+);
+
 
 module.exports = router;
