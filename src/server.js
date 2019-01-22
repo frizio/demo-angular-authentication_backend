92edef607fddd2c6b157b5540a43777ffe1d4047
Maurizio La Rocca <mrz.larocca@gmail.com>
2019-01-22 10:56:49 +0100

    Use cors in server.js

--- src/server.js
+++ src/server.js
@@ -1,9 +1,11 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const api = require('./routes/api');
+const cors = require('cors');
 
 // Initialization
 const app = express();
+app.use(cors());
 
 app.set('port', 3000);
 const port = app.get('port');
