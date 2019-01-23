c729137d10de7be8c84cc53c4218352a847bfc76
Maurizio La Rocca <mrz.larocca@gmail.com>
2019-01-23 11:53:08 +0100

    Define verifyToken middleware function

--- src/routes/api.js
+++ src/routes/api.js
@@ -31,6 +31,21 @@ function generateToken(id) {
     return { token } ;
 }
 
+function verifyToken(req, res, next) {
+    if (!req.headers.authorization) {
+        return res.status(401).send("Unauthorized request");
+    } 
+    let token = req.headers.authorization.split(" ")[1];
+    if (token == 'null') {
+        return res.status(401).send("Unauthorized request");
+    }
+    let payload = jwt.verify(token, secretkey);
+    if (payload == 'null') {
+        return res.status(401).send("Unauthorized request");
+    }
+    req.userId = payload.subject;
+    next();
+}
 
 router.get(
     '/',
